29d5f101409243b794ab4e0dd9021910a6a0ea35
Juan Uys <opyate@gmail.com>
2012-11-10 16:01:20 +0000

    Initial additions - Formations - tileset.png as an asset - underscore Lua

---
+++ src/formation.lua
@@ -0,0 +1,12 @@
+--[[
+-- Formation
+-- Simple grid of on/off values to represent MOAIProp2D instances.
+--]]
+
+Formation = {}
+
+Formation.new = function(props)
+	return (props = props)
+end
+
+-- TODO actual grids, e.g. Tetris shapes, etc
